4dc072a127aa0c3a20224ac7d33a697abc4735f4
Trek <trek@debian>
2017-03-05 21:04:55 -0600

    Update to 2.0

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import sys
-
-if sys.version_info[0] == 2:
-    from commands import getoutput
-elif sys.version_info[0] == 3:
-    from subprocess import getoutput
-
 
 setup(name='pyunifi',
-      version='1.3',
+      version='2.0',
       description='API towards Ubiquity Networks UniFi controller',
       author='Caleb Dunn',
       author_email='finish.06@gmail.com',
@@ -18,4 +11,5 @@ setup(name='pyunifi',
       packages=['pyunifi'],
       scripts=['unifi-low-snr-reconnect', 'unifi-ls-clients', 'unifi-save-statistics', 'unifi-log-roaming'],
       classifiers=[],
+      install_requires=['requests']
      )
